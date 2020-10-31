4bacd50aff3e7bcca974b5ba508bc41eb694f559
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-11-01 00:32:35 +0100

    version 1.0.1

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -65,7 +65,7 @@ const Hero = ({
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Ready for the best music? Check our YouTube channel!
+                Ready for the best music? Check the official YouTube channel!
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
